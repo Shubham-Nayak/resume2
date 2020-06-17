249125bc8c0c504ab0eba22ad53f0044e962d71f
Shubham Nayak <kabirn40@gmail.com>
2020-06-17 18:17:17 +0530

    sada

--- nayak/settings.py
+++ nayak/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shubham',
-    'storages',
-
+    
 ]
 
 MIDDLEWARE = [
@@ -123,19 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'shubham/static'),
 ]
-AWS_ACCESS_KEY_ID = 'AKIA4Y5ZQTWXVKRCLLOG'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'facts24'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
-DEFAULT_FILE_STORAGE = 'nayak.storage_backends.MediaStorage'  # <-- here is where we reference it
+#media MANAGING
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 django_heroku.settings(locals())
- 
